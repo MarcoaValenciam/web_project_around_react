e92ad372004483853c0cb175300703751790dca3
marcoValencia <marco.valencia@outlook.com>
2026-08-13 12:58:55 -0600

    PyS15 v1.3

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -1,18 +1,10 @@
 import { createContext } from 'react';
 
-// const CurrentUserContext = createContext(
-//     currentUser = {
-//         name: "Jacques Cousteau",
-//         about: "Explorer",
-//         avatar: "https://pictures.s3.yandex.net/frontend-developer/common/ava.jpg",
-//     }
-
-
 const CurrentUserContext = createContext({
     currentUser : {
-        name: "",
-        about: "",
-        avatar: "",
+        name: "Jacques Cousteau",
+        about: "Explorer",
+        avatar: "https://pictures.s3.yandex.net/frontend-developer/common/ava.jpg"
     }
 });
 
