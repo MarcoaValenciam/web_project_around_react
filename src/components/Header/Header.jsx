2c797197d13bfb5e578808c8ac8d66ac21087548
marcoValencia <marco.valencia@outlook.com>
2026-08-13 17:31:52 -0600

    PyS15 v1.3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
-import logo from '../../images/logo.png'; // ajusta la ruta según dónde tengas la carpeta images
+import logo from '../../../images/logo.png'; // ajusta la ruta según dónde tengas la carpeta images
 
 export default function Header() {
     return(
         <header className="header">
             <div className="header__logo">
                 <img className="header__logo-image" src={logo} alt="Logo" />
+                alert("Se ha cargado el componente Header y se ha importado la imagen del logo desde: " + logo);
             </div>
         </header>
     );
