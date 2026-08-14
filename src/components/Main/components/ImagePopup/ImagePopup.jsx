4d10b7deb2a7dce19fbe6bd96f3a5ef8e444ed64
marcoValencia <marco.valencia@outlook.com>
2026-08-14 14:47:05 -0600

    Updates

--- src/components/Main/components/ImagePopup/ImagePopup.jsx
+++
@@ -1,9 +0,0 @@
-export default function ImagePopup(props) {
-    const { name, link } = props.card;
-    
-    return (
-        <div className="popup__content">
-            <img className="popup__content-content-image" src={link} alt={name} />
-        </div>
-    );
-}
